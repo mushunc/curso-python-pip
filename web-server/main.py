3f5b2bfe2a4c37640cdb706aa366c189cbfd984c
mushunc <davidvoll_4@hotmail.com>
2024-03-12 10:05:45 -0600

    save docs

--- web-server/main.py
+++ web-server/main.py
@@ -1,17 +1,21 @@
 import store
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 app= FastAPI()
 
 @app.get ('/')
 def get_list():
     return [1,2,3]
 
-@app.get ('/contact')
+@app.get ('/contact', response_class=HTMLResponse)
 def get_list():
-    return {name:'Platzi'}
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>Soy un parrafo</p>
 
-
+     """
+     
+     
 def run():
     store.get_categories()
     
